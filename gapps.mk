4e67c2fec27095727cefff82449d34b99dba6fad
sbwml <984419930@qq.com>
2020-11-29 02:22:27 +0800

    gapps: Add Google Play system updates support

--- gapps.mk
+++ gapps.mk
@@ -99,4 +99,29 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.error.receiver.system.apps=com.google.android.gms \
     ro.atrace.core.services=com.google.android.gms,com.google.android.gms.ui,com.google.android.gms.persistent
 
+# Enable Google Play system updates support
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/gapps/apex
+
+PRODUCT_PACKAGES += \
+    ApexOverlay
+
+# Google Apexes
+PRODUCT_PACKAGES += \
+    com.google.android.adbd \
+    com.google.android.cellbroadcast \
+    com.google.android.conscrypt \
+    com.google.android.extservices \
+    com.google.android.ipsec \
+    com.google.android.media \
+    com.google.android.media.swcodec \
+    com.google.android.mediaprovider \
+    com.google.android.neuralnetworks \
+    com.google.android.os.statsd \
+    com.google.android.permission \
+    com.google.android.resolv \
+    com.google.android.sdkext \
+    com.google.android.telephony \
+    com.google.android.tzdata2
+
 $(call inherit-product, vendor/gapps/common/common-vendor.mk)
